ea15dab0afb665a452af9d0083f711c7dbc48de9
LPredmore <124374222+LPredmore@users.noreply.github.com>
2026-07-21 21:31:20 -0500

    Mount OCS visual explanation modules

--- src/pages/OcsBillingHubPage.tsx
+++ src/pages/OcsBillingHubPage.tsx
@@ -1,11 +1,13 @@
 import OperationClaimsSuccessPage from "./OperationClaimsSuccessPage";
 import { LegacyFormPortal } from "@/components/intake/LegacyFormPortal";
 import { UnifiedOcsRoutingForm } from "@/components/intake/UnifiedOcsRoutingForm";
+import { OcsVisualEnhancements } from "@/components/ocs/OcsVisualEnhancements";
 
 export default function OcsBillingHubPage() {
   return (
     <>
       <OperationClaimsSuccessPage />
+      <OcsVisualEnhancements />
       <LegacyFormPortal targetId="ocs-routing-form">
         <UnifiedOcsRoutingForm />
       </LegacyFormPortal>
