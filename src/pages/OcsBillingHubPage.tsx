325d0d83b92ea1a34807130bdc8d70870e98ff58
LPredmore <124374222+LPredmore@users.noreply.github.com>
2026-07-21 21:39:48 -0500

    Apply OCS visual integration styles

--- src/pages/OcsBillingHubPage.tsx
+++ src/pages/OcsBillingHubPage.tsx
@@ -2,6 +2,7 @@ import OperationClaimsSuccessPage from "./OperationClaimsSuccessPage";
 import { LegacyFormPortal } from "@/components/intake/LegacyFormPortal";
 import { UnifiedOcsRoutingForm } from "@/components/intake/UnifiedOcsRoutingForm";
 import { OcsVisualEnhancements } from "@/components/ocs/OcsVisualEnhancements";
+import "@/components/ocs/ocs-visual-enhancements.css";
 
 export default function OcsBillingHubPage() {
   return (
