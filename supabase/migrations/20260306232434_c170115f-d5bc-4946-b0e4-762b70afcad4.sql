6fde57195686d8de6647870ac801cfaf0478c1de
LPredmore <124374222+LPredmore@users.noreply.github.com>
2026-08-03 06:37:01 -0500

    Remove legacy CRM migration history from website

--- supabase/migrations/20260306232434_c170115f-d5bc-4946-b0e4-762b70afcad4.sql
+++
@@ -1,9 +0,0 @@
-CREATE POLICY "Public can view published long content"
-ON public.posted_content
-FOR SELECT
-TO anon, authenticated
-USING (
-  post_length = 'Long'
-  AND youtube_video_id IS NOT NULL
-  AND status = 'posted'
-);
